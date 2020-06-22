bb03b642e0cc95e16acdecb30352708809e76231
Gustavo Dias <gustavo.diias1@gmail.com>
2020-06-21 23:20:36 -0300

    Terminado front end pagina de cadastro, inicio modal do cadastro

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -29,11 +29,14 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then( (res) => {return res.json() } )
     .then( cities => {
         for(city of cities) {
-            citySelect.innerHTML = citySelect.innerHTML + `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML = citySelect.innerHTML + `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -44,9 +47,55 @@ function getCities(event) {
 
 
 
-
-
-
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+// Itens de coleta
+// pegar todos LI
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    // adicionar ou remover uma classe 
+    itemLi.classList.toggle("selected")
+    
+    const itemId = event.target.dataset.id
+
+    // verificar se existem itens selecionados, se sim
+    // pegar os itens selecionados
+    const alreadySelected = selectedItems.findIndex( function(item) {
+        const itemFound = item == itemId
+        return itemFound 
+    } )
+
+    // se já estiver selecionado, tirar da seleção
+    if(alreadySelected >= 0) {
+        //tirar da seleção
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDiferent = item != itemId
+            return itemIsDiferent
+        })
+
+        selectedItems = filteredItems
+    } else {
+         // se não estiver selecionado,
+         // adicionar a seleção
+        selectedItems.push(itemId)
+    }
+
+    // atualizar o campo escondido com os dados itens selecionados
+    collectedItems.value = selectedItems
+ 
+}
